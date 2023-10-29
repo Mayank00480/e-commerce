305c7006d2a10eec0748e5bf5496cb24048d2ae9
Mayank00480 <mayanksharma1937@gmail.com>
2023-10-29 12:45:26 +0530

    added BootStrap

---
+++ src/component/CardItems/CardItems.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+const CardItems = (props) => {
+  return (
+   <div style = {{display : 'flex', justifyContent : 'space-between'}}>{ props.productsArr.map((items) =>{
+  return  (<Card  className='col-3' >
+    <Card.Img variant="top" src={items.imageUrl} />
+    <Card.Body>
+      <Card.Title style = {{textAlign:'center'}}>{items.title}</Card.Title>
+      <h2 style = {{float : 'left'}}> {items.price}</h2>
+      <Button variant="primary" style = {{float : 'right'}}>Add To Cart</Button>
+    </Card.Body>
+  </Card>)
+   })}</div>
+  )
+}
+
+export default CardItems
---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='row'>
+      <div className='col-12'>
+          <h1>The Generics</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
